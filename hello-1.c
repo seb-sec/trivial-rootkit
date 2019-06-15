0ca38779efc6c7b1c9b5bf96098f32a525de62d6
sc <seb_costache@yahoo.com>
2019-06-14 22:52:35 -0700

    added module info and custom init/exit func names

--- hello-1.c
+++ hello-1.c
@@ -2,15 +2,25 @@
  *	simple kernel module
  */
 
-#include <linux/module.h>
-#include <linux/kernel.h>
+#include <linux/module.h>		/* used by all modules */
+#include <linux/kernel.h>		/* used by KERN_INFO and others */
+#include <linux/init.h>			/* used for macros */
+
+MODULE_LICENCE("GPL");
+MODULE_AUTHOR("me");
+MODULE_DESCRIPTION("simple module")
 
 /* function called on module initialisation */
-int init_module(void) {
-	printk(KERN_ALERT "Hello kernel\n");
+int __init hello_1_init(void) {
+	printk(KERN_INFO "Hello kernel\n");		// find in /var/log/kern.log
 	return 0;
 }
 
-void cleanup_module(void) {
-	printk(KERN_ALERT "Goodbye kernel\n");
+/* function called on module removal, should reverse init function */
+void __exit hello_1_exit(void) {
+	printk(KERN_INFO "Goodbye kernel\n");
 }
+
+/* must call these and pass in our init/exit functions */
+module_init(hello_1_init);
+module_exit(hello_1_exit);
