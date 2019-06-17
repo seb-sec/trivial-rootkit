0b7763c782c1337ac2411e0643b1604f82df0cf0
sc <seb_costache@yahoo.com>
2019-06-17 01:44:02 -0700

    oh god lets not mess with that again

--- Makefile
+++ Makefile
@@ -1,16 +1,4 @@
-MODULE_NAME = not_a_rootkit
-
-SRCS = 	src/not_a_rootkit.c \
-		src/dev_fops.c
-
-INCLUDES = -I$(src)/incl
-
-ccflags-y := $(INCLUDES) 	
-
-OBJS = $(SRCS:.c=.o)
-
-obj-m += $(MODULE_NAME).o
-$(MODULE_NAME)-y = $(OBJS)
+obj-m += not_a_rootkit.o
 
 all:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
---
+++ not_a_rootkit.c
@@ -0,0 +1,124 @@
+/*
+ *	simple kernel module
+ */
+
+#include <linux/module.h>		/* used by all modules */
+#include <linux/kernel.h>		/* used by KERN_INFO and others */
+#include <linux/init.h>			/* used for macros */
+#include <linux/fs.h>			/* used for filesystem stuff */
+#include <linux/uaccess.h>		/* required to include <asm/uaccess.h> */
+#include <asm/uaccess.h>	/* for handling data to userland buffers */
+#include <linux/device.h>		/* for class/device creation */
+#include "dev_fops.h"
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("me");
+MODULE_DESCRIPTION("simple module");
+MODULE_VERSION("0.0.1");
+
+/* prototypes */
+int __init trivial_init(void);
+void __exit trivial_exit(void);
+static int trivial_open(struct inode *, struct file *);
+static int trivial_close(struct inode *, struct file *);
+static ssize_t trivial_write(struct file *, const char *, size_t, loff_t *);
+static ssize_t trivial_read(struct file *, char *, size_t, loff_t *);
+
+
+/* useful defines */
+#define DEVICE_NAME "not_a_rootkit"		// name appears in /proc/devices
+#define CLASS_NAME 	"not_a"
+
+/* globals (within this file) */
+static int majorNum;					// device major number assigned
+static int deviceOpen = 0;				// track if device open, may be useful later
+static struct class *devClass = NULL;
+static struct device *devStruct = NULL;
+
+/* used to store any data aquired, given back during a read()- needs to be allocated */
+static char *deviceBuf = NULL;			
+
+/* must declare file system operations that device can perform- see linux/fs.h */
+static struct file_operations fops = {
+	.read = trivial_read,
+	.write = trivial_write,
+	.open = trivial_open,
+	.release = trivial_close
+};
+
+/*---------------- startup/shutdown ------------------------*/
+/* function called on module initialisation */
+int __init trivial_init(void) {
+
+	/* register our device with the kernel */
+	majorNum = register_chrdev(0, DEVICE_NAME, &fops);
+
+	if (majorNum < 0) {
+		printk(KERN_ALERT "Failed to register rootkit chardevice\n");
+		return majorNum;
+	}
+	
+	/* register the device class */
+	devClass = class_create(THIS_MODULE, CLASS_NAME);
+	if (IS_ERR(devClass)) {
+		unregister_chrdev(majorNum, DEVICE_NAME);
+		printk(KERN_ALERT "Failed to register rootkit char class\n");
+		return -1;		// tmp value
+	}
+
+	/* create the device */
+	devStruct = device_create(devClass, NULL, MKDEV(majorNum, 0), NULL, DEVICE_NAME);
+	if (IS_ERR(devStruct)) {
+		class_destroy(devClass);
+		unregister_chrdev(majorNum, DEVICE_NAME);
+		printk(KERN_ALERT "Failed to create rootkit device\n");
+		return -1;		// tmp value
+	}
+
+	printk(KERN_INFO "Registered rootkit with the kernel. Thanks kernel\n");
+	/* probably a whole lot of stealth work here */
+
+	return 0;
+}
+
+/* function called on module removal, should reverse init function */
+void __exit trivial_exit(void) {
+	
+	/* unregister the device, cover our traces */
+	device_destroy(devClass, MKDEV(majorNum, 0));
+	class_unregister(devClass);
+	class_destroy(devClass);
+	unregister_chrdev(majorNum, DEVICE_NAME);
+}
+
+static int trivial_open(struct inode *inode, struct file *f) {
+	printk(KERN_INFO "Inconspicuous device opened\n");
+	return 0;
+}
+
+/* not sure if this is required, may have some use for this in future though */
+static int trivial_close(struct inode *inode, struct file *f) {
+	return 0;
+}
+
+/* 
+ * called when a process writes to the device, used to execute specific commands 
+ * expects the amount of bytes written returned
+ */
+static ssize_t trivial_write(struct file *f, const char *buff, size_t len, loff_t *off) {
+	printk(KERN_INFO "Write performed to inconspicuous device\n");
+	return len;
+}
+
+/*
+ * called when a process reads from the device, expects number of bytes read to be returned
+ * maybe we give back custom data (say, from a command given to write()) in future
+ */
+static ssize_t trivial_read(struct file *f, char *buff, size_t length, loff_t *offset) {
+	printk(KERN_INFO "Read performed on inconspicuous device\n");
+	return 0;		// signal EOF for now
+}
+
+/* must call these and pass in our init/exit functions */
+module_init(trivial_init);
+module_exit(trivial_exit);
